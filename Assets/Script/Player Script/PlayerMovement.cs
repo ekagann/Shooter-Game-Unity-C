a782b0132130b0cb0cb64a1a330b7b373aa948ba
EKT <efekagantelevi@gmail.com>
2023-06-06 15:18:59 +0300

    player movement+animations

--- Assets/Script/Player Script/PlayerMovement.cs
+++ Assets/Script/Player Script/PlayerMovement.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float moveSpeed = 3.5f;
+    [SerializeField]
+    private float minBound_X = -71f, maxBound_X = 71f, minBound_Y = -3.3f, maxBound_Y = 0f;
+
+    private Vector3 tempPos;
+
+    private float xAxis, yAxis;
+
 
     void Update()
     {
-        
+        HandleMovement();
+    }
+    void HandleMovement()
+    {
+        xAxis = Input.GetAxisRaw(TagManager.HORIZONTAL_AXÝS);
+        yAxis = Input.GetAxisRaw(TagManager.VERTICAL_AXÝS);
+
+        tempPos = transform.position;
+
+        tempPos.x += xAxis * moveSpeed * Time.deltaTime;
+        tempPos.y += yAxis * moveSpeed * Time.deltaTime;
+
+        if (tempPos.x < minBound_X)
+            tempPos.x = minBound_X;
+        else if (tempPos.x > maxBound_X)
+            tempPos.x = maxBound_X;
+
+        if (tempPos.y < minBound_Y)
+            tempPos.y = minBound_Y;
+        else if (tempPos.y > maxBound_Y)
+            tempPos.y = maxBound_Y;
+
+        transform.position = tempPos;
     }
 } // class
 
